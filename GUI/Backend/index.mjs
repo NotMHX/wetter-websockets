87c4dffb11130cd17038de74c68410576a7b9ae0
LucaDalCorso2004 <L.DalCorso.inf21@stud.bbbaden.ch>
2024-03-26 15:27:23 +0100

    Function finished

--- GUI/Backend/index.mjs
+++ GUI/Backend/index.mjs
@@ -8,22 +8,33 @@ let cities = ["Zürich", "Berlin", "Vladivostok", "Albuquerque"];
 
 let newId = 0;
 
-wss.on("connection", (ws) => {
+wss.on("connection", async (ws) => {
   console.log("New client connected");
 
+
+  const data = await sendAllData();
+  console.log(data)
+  ws.send(JSON.stringify(data));
+
   ws.on("message", (msg) => {
-    switch (msg.type) {
+
+    let location = JSON.parse(msg)
+
+
+    console.log(location)
+    switch (location.type) {
       case "login": // existiert noch nicht
         users.push({
           id: newId,
-          name: msg.content,
+          name: location.content,
         });
 
         newId++;
         break;
 
       case "addLocation":
-        cities.push(msg.content);
+        console.log("Here")
+        cities.push(location.content);
         break;
 
       default:
@@ -38,7 +49,14 @@ wss.on("connection", (ws) => {
     console.log("Client has disconnected");
   });
 
-  ws.send("test");
+
+
+
+  setInterval(async () => {
+    const data = await sendAllData();
+    console.log(data)
+    ws.send(JSON.stringify(data));
+  }, 5000);
 });
 
 async function getWeather(city) {
@@ -47,6 +65,8 @@ async function getWeather(city) {
   );
   const cityJson = await cityData.json();
   const cityJson2 = cityJson.results[0];
+
+
 
   let weatherData = await fetch(
     `https://api.open-meteo.com/v1/forecast?latitude=${cityJson2.latitude}&longitude=${cityJson2.longitude}&hourly=temperature_2m&timezone=Europe%2FBerlin&forecast_days=3`
@@ -57,20 +77,21 @@ async function getWeather(city) {
 }
 
 async function sendAllData() {
+  let weatherCity = [];
   for (const city of cities) {
     try {
-      let weatherCity = await getWeather(city);
-      console.log(
-        `In ${city}: At ${weatherCity.hourly.time[0]} the temperature is ${weatherCity.hourly.temperature_2m[0]}`
-      );
+      let cityWeatherObject = {
+        city: city,
+        data: await getWeather(city),
+      };
+      weatherCity.push(cityWeatherObject);
     } catch {
       console.log(`City ${city} not found.`);
     }
   }
+  return weatherCity;
 }
 
 (async function () {
-  setInterval(async () => {
-    await sendAllData();
-  }, 10000);
+
 })(); // alle 10 sekunden
